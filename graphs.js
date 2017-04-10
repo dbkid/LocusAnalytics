da7655aacff368b43476c7a6905532a2079fc566
Danielle Butera Keefe <dbkeefe@gmail.com>
2017-04-10 12:04:50 -0400

    fix spacing

--- graphs.js
+++ graphs.js
@@ -1,6 +1,5 @@
 d3.csv("./Locus_seattle_aerospace.csv", function(error, data) {
 
-
   var margin = {top: 20, right: 20, bottom: 50, left: 150},
   width = 960 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
@@ -13,7 +12,7 @@ d3.csv("./Locus_seattle_aerospace.csv", function(error, data) {
     .style("opacity", 0)
     .attr("class", "popup")
 
-  //create initial svg 
+  //create initial svg
   var svg = d3.select(".chart-container").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
